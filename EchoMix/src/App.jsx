f2c989e2fadcf16e6b971797b16a37b87630149b
gourav <sonu1mission@gmail.com>
2025-04-30 01:11:33 +0530

    Revert "LoginSignup"

--- EchoMix/src/App.jsx
+++ EchoMix/src/App.jsx
@@ -2,16 +2,13 @@ import React from 'react';
 import Aboutus from './components/Aboutus/Aboutus'; 
 import Home from './components/Home/Home';
 import NavBar from './components/NavBar/NavBar';
-import LoginSignup from './components/LoginSignup/LoginSignup';
-
 
 import Song from './components/song/Song'
 import { Nav } from 'react-bootstrap';
 function App() {
   return (
     <>
-  
-     <LoginSignup></LoginSignup>
+     <Home></Home>
     </>
   );
 }
